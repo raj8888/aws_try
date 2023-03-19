15f4ad65778d478715debd7531d3217dc139447f
raj8888 <jadhavrj8877@gmail.com>
2023-03-19 21:55:43 +0530

    make some changes

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,7 +2,7 @@ const express=require('express')
 const bcrypt=require("bcrypt")
 const jwt=require("jsonwebtoken")
 const {sequelize}=require("../config/server")
-const {userModel}=require("../model/user.model")
+const {userModel}=require("../models/user.model")
 const userRouter=express.Router()
 
 userRouter.post("/signup",async(req,res)=>{
